4f584e3c3e783f06835a0132b225123469e4075a
Gomam0n <603934493@qq.com>
2021-07-25 21:47:57 +0800

    listen to port

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+)
 
-func main (){
+var IP = "127.0.0.1"
+var PORT = 8888
+
+func main() {
 	fmt.Println("Hello world")
+	address := IP + ":" + strconv.Itoa(PORT)
+	addr, err := net.ResolveUDPAddr("udp", address)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	conn, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	defer conn.Close()
 }
