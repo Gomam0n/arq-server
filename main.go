17b33467df812ee68cde0125d7cf5366b89ff642
Gomam0n <603934493@qq.com>
2021-07-31 18:58:41 +0800

    fucntions

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"net"
 	"os"
 	"strconv"
+	"strings"
+	"time"
 )
 
 var IP = "127.0.0.1"
@@ -27,7 +29,6 @@ func main() {
 	defer conn.Close()
 
 	for {
-
 		data := make([]byte, 16)
 		len, addr, err := conn.ReadFromUDP(data)
 		if err != nil {
@@ -41,24 +42,25 @@ func transport(addr *net.UDPAddr, data string) {
 	fmt.Println("Connection from " + addr.String())
 
 	conn, err := DialClient(addr, data)
+
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	clientData := make([]byte, 16)
-	fmt.Println(clientData)
 
+	clientData := make([]byte, 17)
+	var sendData []byte
 	udpAddr, err := getUDPAddr(0)
+
+	fmt.Println(udpAddr.Port, udpAddr.IP, udpAddr.Network())
+
 	listenConn, err := net.ListenUDP("udp", udpAddr)
-	if err == nil {
+	if err != nil {
 		return
 	}
 	defer listenConn.Close()
-
-	ack := "Ready to sent"
-	conn.Write([]byte(ack))
-
-	// the first re-transmission logic
+	portUse := listenConn.LocalAddr().String()[strings.Index(listenConn.LocalAddr().String(), ":")+1:]
+	var ch1 = make(chan []byte, 2)
 
 	content, err := ReadFile(filename)
 	if err != nil {
@@ -66,13 +68,78 @@ func transport(addr *net.UDPAddr, data string) {
 		return
 	}
 
-	for i := 0; i < len(content); i += 16 {
-		if i+16 < len(content) {
-			conn.Write(content[i : i+16])
-		} else {
-			conn.Write(content[i:])
+	go ListenClient(listenConn, clientData, ch1)
+
+	sendData = []byte(portUse)
+	conn.Write(sendData)
+	fmt.Println(conn.RemoteAddr())
+	fmt.Println("use port ", portUse, " to communicate with ", conn.RemoteAddr())
+
+	sequenceNumber := 0
+	i := 0
+	//timeout := true
+	fmt.Println(sequenceNumber, i)
+	timer := time.NewTimer(time.Millisecond * 300)
+	for {
+		// timer may be not active, and fired
+		if !timer.Stop() {
+			select {
+			case <-timer.C: //try to drain from the channel
+			default:
+			}
+		}
+		timer.Reset(time.Millisecond * 300)
+		select {
+		case clientData := <-ch1:
+			fmt.Println("reveive ", string(clientData), " from ", conn.RemoteAddr())
+			if clientData[0] == 'A' && clientData[1] == 'C' && clientData[2] == 'K' && clientData[3] == byte(sequenceNumber+'0') {
+				if i > len(content) {
+					break
+				} else {
+					sendData = nil
+					sequenceNumber = 1 - sequenceNumber
+					sendData = append(sendData, byte(sequenceNumber+'0'))
+					if i+16 > len(content) {
+						sendData = append(sendData, content[i:]...)
+					} else {
+						sendData = append(sendData, content[i:i+16]...)
+					}
+					i += 16
+					conn.Write(sendData)
+					fmt.Println("send next packet to ", conn.RemoteAddr())
+					continue
+				}
+			}
+		case <-timer.C:
+			fmt.Println(time.Now(), ":timer expired")
+			conn.Write(sendData)
+			continue
 		}
 	}
+	/*
+		for{
+			if clientData[0] == 'A' &&clientData[1] == 'C' &&clientData[2] == 'K'&& clientData[3] == byte(sequenceNumber+'0') {
+				clientData[0],clientData[1],clientData[2],clientData[3] = '0','0','0','0'
+				if i>len(content){
+					break
+				}else{
+					sendData = nil
+					sequenceNumber = ^sequenceNumber
+					sendData= append(sendData,byte(sequenceNumber+'0') )
+					sendData = append(sendData,content[i:i+16]...)
+					i+=16
+					conn.Write(sendData)
+					timer.Stop()
+					timer.Reset(time.Millisecond*300)
+				}
+			}else if timeout == true{
+				conn.Write(sendData)
+				timer.Reset(time.Millisecond*300)
+			}
+
+		}
+	*/
+
 	return
 }
 
@@ -101,6 +168,7 @@ func DialClient(addr *net.UDPAddr, data string) (conn net.Conn, err error) {
 		return
 	}
 	clientAddr := addr.IP.String() + ":" + strconv.Itoa(int(port))
+	// error?
 	conn, err = net.Dial("udp", clientAddr)
 	if err != nil {
 		fmt.Println("The connection with " + addr.IP.String() + " has error: " + err.Error())
@@ -108,11 +176,30 @@ func DialClient(addr *net.UDPAddr, data string) (conn net.Conn, err error) {
 	return
 }
 
-func ListenClient(addr *net.UDPAddr, clientData *[]byte) {
-
+func ListenClient(conn *net.UDPConn, clientData []byte, ch1 chan []byte) {
+	fmt.Println(conn.LocalAddr())
+	for {
+		len, _, err := conn.ReadFromUDP(clientData)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		fmt.Println(string(clientData[:len]))
+		ch1 <- clientData[:len]
+		clientData = make([]byte, 17)
+	}
 }
 func getUDPAddr(port int) (udpAddr *net.UDPAddr, err error) {
 	serverAddress := IP + ":" + strconv.Itoa(port)
 	udpAddr, err = net.ResolveUDPAddr("udp", serverAddress)
 	return
+}
+func count(cnt *int) {
+	timer := time.NewTimer(time.Millisecond)
+	for {
+		<-timer.C
+		timer.Reset(time.Millisecond)
+		*cnt++
+		fmt.Println(*cnt)
+	}
 }
